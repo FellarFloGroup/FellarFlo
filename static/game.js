0678dcda8fce3825482b2202fbab1b1cc16c73fe
evanfellman <evan.fellman@gmail.com>
2021-03-09 15:53:57 -0500

    started improving phone UI

--- static/game.js
+++ static/game.js
@@ -1,3 +1,11 @@
+let isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+    isMobile = true;
+}
+
+
 class Pixel{
 	constructor(color="empty"){
 		if(color === "empty"){
@@ -159,7 +167,7 @@ sections.style.position = 'fixed';
 sections.style.top = '0px';
 sections.style.left = '0px';
 sections.style.width = '100%';
-sections.style.height = '100%';
+sections.style.height = '75%';
 document.body.appendChild(sections);
 const sectionsBody = document.createElement('tbody');
 sections.appendChild(sectionsBody);
@@ -523,10 +531,8 @@ let gameInterval = setInterval(() => {
 	score += rowsCleared * rowsCleared;
 }, SPEED_DOWNWARDS);
 
-document.onkeydown = function (e) {
-    e = e || window.event;
-	// use e.keyCode
-    if (e.keyCode == '40') {
+function move(str){
+	if(str === 'down'){
 		let change = 1;
 	   	for(let i = 0; i < playerPiece.piece.piece.length; i++){
 			for(let j = 0; j < playerPiece.piece.piece[i].length; j++){
@@ -538,7 +544,7 @@ document.onkeydown = function (e) {
 			}
 		}
 		playerPiece.y -= change;
-    } else if (e.keyCode == '37') {
+	} else if(str === 'left'){
 		let change = 1;
 	   	for(let i = 0; i < playerPiece.piece.piece.length; i++){
 			for(let j = 0; j < playerPiece.piece.piece[i].length; j++){
@@ -550,7 +556,7 @@ document.onkeydown = function (e) {
 			}
 		}
 		playerPiece.x -= change;
-    } else if (e.keyCode == '39') {
+	} else if(str === 'right'){
 		let change = 1;
 	   	for(let i = 0; i < playerPiece.piece.piece.length; i++){
 			for(let j = 0; j < playerPiece.piece.piece[i].length; j++){
@@ -562,9 +568,85 @@ document.onkeydown = function (e) {
 			}
 		}
 		playerPiece.x += change;
+	}
+}
+
+document.onkeydown = function (e) {
+    e = e || window.event;
+	// use e.keyCode
+    if (e.keyCode == '40') {
+		move("down");
+    } else if (e.keyCode == '37') {
+		move("left");
+    } else if (e.keyCode == '39') {
+		move("right");
 	} else if(e.key === 'a'){
 		rotatePlayerPiece(playerPiece, 'left');
 	} else if(e.key === 'd'){
 		rotatePlayerPiece(playerPiece, 'right');
 	}
 };
+
+if(isMobile){
+	const bottomSection = document.createElement('table');
+	bottomSection.style.position = 'fixed';
+	bottomSection.style.bottom = 0;
+	document.body.appendChild(bottomSection);
+	bottomSection.style.width = "100%";
+	const rtLft = document.createElement("td");
+	const rotateLeftBtn = document.createElement('div');
+	rotateLeftBtn.style.backgroundColor = 'white';
+	rotateLeftBtn.style.width = screen.width * 0.25;
+	rotateLeftBtn.innerHTML = "<h1>↩</h1>";
+	rotateLeftBtn.style.cursor = "pointer";
+	rotateLeftBtn.style.textAlign = 'center';
+	rotateLeftBtn.onclick = () => rotatePlayerPiece(playerPiece, 'left');
+	bottomSection.appendChild(rtLft);
+	rtLft.appendChild(rotateLeftBtn);
+
+	const rtRght = document.createElement('td');
+	const rotateRightBtn = document.createElement('div');
+	rotateRightBtn.style.backgroundColor = 'white';
+	rotateRightBtn.style.width = screen.width * 0.25;
+	rotateRightBtn.innerHTML = "<h1>↪</h1>";
+	rotateRightBtn.style.cursor = "pointer";
+	rotateRightBtn.style.textAlign = 'center';
+	rotateRightBtn.onclick = () => rotatePlayerPiece(playerPiece, 'right');
+	rtRght.appendChild(rotateRightBtn);
+	bottomSection.appendChild(rtRght);
+
+
+	const mvLft = document.createElement('td');
+	mvLft.style.paddingLeft =  screen.width * 0.025;
+	const moveLeftBtn = document.createElement('div');
+	moveLeftBtn.style.backgroundColor = 'white';
+	moveLeftBtn.style.width =  screen.width * 0.15;
+	moveLeftBtn.innerHTML = "<h1>◀</h1>";
+	moveLeftBtn.style.cursor = "pointer";
+	moveLeftBtn.style.textAlign = 'center';
+	moveLeftBtn.onclick = () => move("left");
+	mvLft.appendChild(moveLeftBtn);
+	bottomSection.appendChild(mvLft);
+
+	const mvDwn = document.createElement('td');
+	const moveDownBtn = document.createElement('div');
+	moveDownBtn.style.backgroundColor = 'white';
+	moveDownBtn.style.width = screen.width * 0.15;
+	moveDownBtn.innerHTML = "<h1>🔽</h1>";
+	moveDownBtn.style.cursor = "pointer";
+	moveDownBtn.style.textAlign = 'center';
+	moveDownBtn.onclick = () => move("down");
+	mvDwn.appendChild(moveDownBtn);
+	bottomSection.appendChild(mvDwn);
+
+	const mvRght = document.createElement('td');
+	const moveRightBtn = document.createElement('div');
+	moveRightBtn.style.backgroundColor = 'white';
+	moveRightBtn.style.width = screen.width * 0.15;
+	moveRightBtn.innerHTML = "<h1>▶</h1>";
+	moveRightBtn.style.cursor = "pointer";
+	moveRightBtn.style.textAlign = 'center';
+	moveRightBtn.onclick = () => move("down");
+	mvRght.appendChild(moveRightBtn);
+	bottomSection.appendChild(mvRght);
+}
